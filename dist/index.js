6e230ed4eb034614245d1ba7c63f9c0fb59bb5ea
Rye Nguyen <ryenguyen7411@gmail.com>
2021-07-11 19:25:46 +0700

    UPDATE: handle indexedDB.

--- dist/index.js
+++ dist/index.js
@@ -5,15 +5,109 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
+var _idb = require("idb");
+
 var _generate = _interopRequireDefault(require("nanoid/generate"));
 
-var _jsCookie = _interopRequireDefault(require("js-cookie"));
+require("regenerator-runtime/runtime");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-var SEED = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var SEED = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+function handleRequest(protocol, params) {
+  var method, _localStorage, _localStorage2, _localStorage3, db, res, _res, _res2;
+
+  return regeneratorRuntime.async(function handleRequest$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          method = protocol.split('CrossStorage:')[1];
+
+          if (!(method === 'getItem')) {
+            _context.next = 3;
+            break;
+          }
+
+          return _context.abrupt("return", (_localStorage = localStorage).getItem.apply(_localStorage, params));
+
+        case 3:
+          if (!(method === 'setItem')) {
+            _context.next = 5;
+            break;
+          }
+
+          return _context.abrupt("return", (_localStorage2 = localStorage).setItem.apply(_localStorage2, params));
+
+        case 5:
+          if (!(method === 'removeItem')) {
+            _context.next = 7;
+            break;
+          }
+
+          return _context.abrupt("return", (_localStorage3 = localStorage).removeItem.apply(_localStorage3, params));
+
+        case 7:
+          _context.next = 9;
+          return regeneratorRuntime.awrap((0, _idb.openDB)('cross-storage', 1, {
+            upgrade: function upgrade(db) {
+              return db.createObjectStore('generic');
+            }
+          }));
+
+        case 9:
+          db = _context.sent;
+
+          if (!(method === 'getIndexed')) {
+            _context.next = 16;
+            break;
+          }
+
+          _context.next = 13;
+          return regeneratorRuntime.awrap(db.get.apply(db, ['generic'].concat(params)));
+
+        case 13:
+          res = _context.sent;
+          db.close();
+          return _context.abrupt("return", res);
+
+        case 16:
+          if (!(method === 'setIndexed')) {
+            _context.next = 22;
+            break;
+          }
+
+          _context.next = 19;
+          return regeneratorRuntime.awrap(db.put('generic', params[1], params[0]));
+
+        case 19:
+          _res = _context.sent;
+          db.close();
+          return _context.abrupt("return", _res);
+
+        case 22:
+          if (!(method === 'removeIndexed')) {
+            _context.next = 28;
+            break;
+          }
+
+          _context.next = 25;
+          return regeneratorRuntime.awrap(db.delete.apply(db, ['generic'].concat(params)));
+
+        case 25:
+          _res2 = _context.sent;
+          db.close();
+          return _context.abrupt("return", _res2);
+
+        case 28:
+        case "end":
+          return _context.stop();
+      }
+    }
+  }, null, null, null, Promise);
+}
 
 var CrossStorage = function CrossStorage() {
   _defineProperty(this, "open", function (allowedDomains) {
@@ -21,109 +115,103 @@ var CrossStorage = function CrossStorage() {
       allowedDomains = [];
     }
 
-    this.__rootListener = function (message) {
-      var origin = message.origin === "null" ? "file://" : message.origin;
-      var isAllowed = allowedDomains.some(function (regx) {
-        if (!regx instanceof RegExp) return false;
-        return regx.test(origin);
-      });
-      if (!isAllowed) return;
+    this.__rootListener = function _callee(message) {
+      var origin, isAllowed, req, res;
+      return regeneratorRuntime.async(function _callee$(_context2) {
+        while (1) {
+          switch (_context2.prev = _context2.next) {
+            case 0:
+              origin = message.origin === 'null' ? 'file://' : message.origin;
+              isAllowed = allowedDomains.some(function (regx) {
+                if (!(regx instanceof RegExp)) return false;
+                return regx.test(origin);
+              });
 
-      var req = function () {
-        try {
-          return JSON.parse(message.data);
-        } catch (err) {
-          return;
+              if (isAllowed) {
+                _context2.next = 4;
+                break;
+              }
+
+              return _context2.abrupt("return");
+
+            case 4:
+              req = function () {
+                try {
+                  return JSON.parse(message.data);
+                } catch (_unused) {}
+              }();
+
+              if (req) {
+                _context2.next = 7;
+                break;
+              }
+
+              return _context2.abrupt("return");
+
+            case 7:
+              res = {
+                id: req.id
+              };
+
+              try {
+                res.result = handleRequest(req.method, req.params);
+              } catch (err) {
+                res.error = err;
+              }
+
+              window.parent.postMessage(JSON.stringify(res), origin);
+
+            case 10:
+            case "end":
+              return _context2.stop();
+          }
         }
-      }();
-
-      if (!req) return;
-
-      var res = function () {
-        try {
-          var method = req.method.split("CrossStorage:")[1];
-
-          var result = function () {
-            var _localStorage, _localStorage2, _localStorage3;
-
-            switch (method) {
-              case 'getItem':
-                return (_localStorage = localStorage).getItem.apply(_localStorage, req.params);
-
-              case 'setItem':
-                return (_localStorage2 = localStorage).setItem.apply(_localStorage2, req.params);
-
-              case 'removeItem':
-                return (_localStorage3 = localStorage).removeItem.apply(_localStorage3, req.params);
-
-              case 'getCookie':
-                return _jsCookie.default.get.apply(_jsCookie.default, req.params);
-
-              case 'setCookie':
-                return _jsCookie.default.set.apply(_jsCookie.default, req.params);
-
-              case 'removeCookie':
-                return _jsCookie.default.remove.apply(_jsCookie.default, req.params);
-
-              default:
-                return;
-            }
-          }();
-
-          return JSON.stringify({
-            id: req.id,
-            result: result
-          });
-        } catch (err) {
-          return JSON.stringify({
-            id: req.id,
-            error: err
-          });
-        }
-      }();
-
-      window.parent.postMessage(res, origin);
+      }, null, null, null, Promise);
     };
 
     if (window.addEventListener) {
-      window.addEventListener("message", this.__rootListener.bind(this), false);
+      window.addEventListener('message', this.__rootListener.bind(this), false);
     } else {
-      window.attachEvent("onmessage", this.__rootListener.bind(this));
+      window.attachEvent('onmessage', this.__rootListener.bind(this));
     }
   });
 
   _defineProperty(this, "close", function () {
     if (window.removeEventListener) {
-      window.removeEventListener("message", this.__rootListener, false);
+      window.removeEventListener('message', this.__rootListener, false);
     } else {
-      window.detachEvent("onmessage", this.__rootListener);
+      window.detachEvent('onmessage', this.__rootListener);
     }
   });
 
   _defineProperty(this, "connect", function (rootDomain, _temp) {
-    var _this = this;
+    var _this2 = this;
 
     var _ref = _temp === void 0 ? {} : _temp,
         _ref$frameId = _ref.frameId,
-        frameId = _ref$frameId === void 0 ? "cross-storage" : _ref$frameId,
-        callback = _ref.callback;
+        frameId = _ref$frameId === void 0 ? 'cross-storage' : _ref$frameId;
 
-    if (this.__connectionStatus === "CONNECTED") {
-      typeof callback === "function" && callback(this);
+    if (rootDomain === 'local') return this.connectLocal();
+
+    if (this.__connectionStatus === 'CONNECTED') {
       return this;
     }
 
     var frame = document.getElementById(frameId);
-    if (frame) return console.error("ERROR: " + frameId + " has already be used. Please provide different frameId");
-    frame = window.document.createElement("iframe");
+
+    if (frame) {
+      return console.error("ERROR: " + frameId + " has already be used. Please provide different frameId");
+    }
+
+    frame = window.document.createElement('iframe');
     this.__frame = frame;
     this.__frame.id = frameId;
-    this.__frame.style["display"] = "none";
+    this.__frame.style.display = 'none';
     window.document.body.appendChild(this.__frame);
     this.__frame.src = rootDomain;
 
     this.__clientListener = function (message) {
-      var origin = message.origin === "null" ? "file://" : message.origin;
+      var origin = message.origin === 'null' ? 'file://' : message.origin;
       var isAllowed = origin === rootDomain;
       if (!isAllowed) return;
 
@@ -133,93 +221,104 @@ var CrossStorage = function CrossStorage() {
         } catch (err) {}
       }();
 
-      if (!res || !res.id || typeof this.__requests[res.id] !== "function") return;
+      if (!res || !res.id || typeof this.__requests[res.id] !== 'function') return;
 
       this.__requests[res.id](res.error, res.result);
     };
 
+    this.__request = function (method, params) {
+      var _this = this;
+
+      if (this.__connectionStatus !== 'CONNECTED') {
+        return console.error('ERROR: CrossStorage has not been up yet.');
+      }
+
+      var req = {
+        id: (0, _generate.default)(SEED, 10),
+        method: 'CrossStorage:' + method,
+        params: params
+      };
+      return new Promise(function (resolve, reject) {
+        _this.__timeout = setTimeout(function () {
+          if (!_this.__requests[req.id]) return;
+          delete _this.__requests[req.id];
+          reject(new Error('timeout'));
+        }, 5000);
+
+        _this.__requests[req.id] = function (err, result) {
+          clearTimeout(_this.__timeout);
+          delete _this.__requests[req.id];
+          if (err) return reject(new Error(err));
+          resolve(result);
+        };
+
+        _this.__frame.contentWindow.postMessage(JSON.stringify(req), _this.__frame.src);
+      }).catch(function (e) {
+        console.error('ERROR: ', e);
+      });
+    };
+
     if (window.addEventListener) {
-      window.addEventListener("message", this.__clientListener.bind(this), false);
+      window.addEventListener('message', this.__clientListener.bind(this), false);
     } else {
-      window.attachEvent("onmessage", this.__clientListener.bind(this));
+      window.attachEvent('onmessage', this.__clientListener.bind(this));
     }
 
     return new Promise(function (resolve) {
-      _this.__frame.onload = function () {
-        _this.__connectionStatus = "CONNECTED";
-        _this.__requests = {};
-
-        _this.__request = function (method, params) {
-          var _this2 = this;
-
-          if (this.__connectionStatus !== "CONNECTED") {
-            return console.error("ERROR: CrossStorage has not been up yet.");
-          }
-
-          var req = {
-            id: (0, _generate.default)(SEED, 10),
-            method: "CrossStorage:" + method,
-            params: params
-          };
-          return new Promise(function (resolve, reject) {
-            _this2.__timeout = setTimeout(function () {
-              if (!_this2.__requests[req.id]) return;
-              delete _this2.__requests[req.id];
-              reject(new Error("timeout"));
-            }, 5000);
-
-            _this2.__requests[req.id] = function (err, result) {
-              clearTimeout(_this2.__timeout);
-              delete _this2.__requests[req.id];
-              if (err) return reject(new Error(err));
-              resolve(result);
-            };
-
-            _this2.__frame.contentWindow.postMessage(JSON.stringify(req), _this2.__frame.src);
-          }).catch(function (e) {
-            console.error("ERROR: ", e);
-          });
-        };
-
-        typeof callback === "function" && callback(_this);
-        resolve(_this);
+      _this2.__frame.onload = function () {
+        _this2.__connectionStatus = 'CONNECTED';
+        _this2.__requests = {};
+        resolve(_this2);
       };
     });
   });
 
+  _defineProperty(this, "connectLocal", function () {
+    this.__request = function (method, params) {
+      var req = {
+        id: (0, _generate.default)(SEED, 10),
+        method: 'CrossStorage:' + method,
+        params: params
+      };
+      return handleRequest(req.method, params);
+    };
+
+    return this;
+  });
+
   _defineProperty(this, "disconnect", function () {
     if (this.__frame) this.__frame.parentNode.removeChild(this.__frame);
-    this.__connectionStatus = "DISCONNECTED";
+    this.__connectionStatus = 'DISCONNECTED';
 
     if (window.removeEventListener) {
-      window.removeEventListener("message", this.__clientListener, false);
+      window.removeEventListener('message', this.__clientListener, false);
     } else {
-      window.detachEvent("onmessage", this.__clientListener);
+      window.detachEvent('onmessage', this.__clientListener);
     }
   });
 
   _defineProperty(this, "getItem", function (key) {
-    return this.__request("getItem", [key]);
+    return this.__request('getItem', [key]);
   });
 
   _defineProperty(this, "setItem", function (key, value) {
-    return this.__request("setItem", [key, value]);
+    return this.__request('setItem', [key, value]);
   });
 
   _defineProperty(this, "removeItem", function (key) {
-    return this.__request("removeItem", [key]);
+    return this.__request('removeItem', [key]);
   });
 
-  _defineProperty(this, "getCookie", function (key) {
-    return this.__request("getCookie", [key]);
+  _defineProperty(this, "getIndexed", function (key) {
+    return this.__request('getIndexed', [key]);
   });
 
-  _defineProperty(this, "setCookie", function (key, value) {
-    return this.__request("setCookie", [key, value]);
+  _defineProperty(this, "setIndexed", function (key, value) {
+    return this.__request('setIndexed', [key, value]);
   });
 
-  _defineProperty(this, "removeCookie", function (key) {
-    return this.__request("removeCookie", [key]);
+  _defineProperty(this, "removeIndexed", function (key, value) {
+    return this.__request('removeIndexed', [key, value]);
   });
 };
 
